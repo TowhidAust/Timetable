c4c420345090f807b3db26f4f47601c3f4346f3c
TowhidAust <towhidaustcse33@gmail.com>
2018-11-24 16:53:21 +0600

    functionality fixed

--- js/timetable.js
+++ js/timetable.js
@@ -3,15 +3,14 @@ function timeTableGrid(sTime, eTime, day) {
   const offsetEndTime = $(eTime).offset();
   const offsetDay = $(day).offset();
 
-  const x1 = parseFloat(offsetStartTime.left);
-  const y1 = parseFloat(offsetStartTime.top);
-  const x2 = parseFloat(offsetEndTime.left);
-  const y2 = parseFloat(offsetEndTime.top);
+  const x1 = offsetStartTime.left;
+  const y1 = offsetStartTime.top;
+  const x2 = offsetEndTime.left;
+  const y2 = offsetEndTime.top;
 
-  const dx1 = parseFloat(offsetDay.left);
-  const dy1 = parseFloat(offsetDay.top);
+  const dx1 = offsetDay.left;
+  const dy1 = offsetDay.top;
   console.log(x1);
-
   console.log(
     "x1: " + x1,
     "y1: " + y1,
@@ -21,26 +20,20 @@ function timeTableGrid(sTime, eTime, day) {
     "dy1: " + dy1
   );
   const calcHeight = y2 - y1;
-  // const calcHeightForMiddleAlign = (y2 - y1) / 2;
-
   console.log("height: " + calcHeight);
-  // placing a div into a new position
-  $("button").click(function() {
-    newPos = new Object();
-    newPos.left = dx1;
-    newPos.top = y1;
-    console.log("Loop executed");
-    $(".div1").offset(newPos);
-
-    $(".div1").css("height", calcHeight);
-    parentWidth = $(".day").innerWidth();
-    $(".div1").css("width", parentWidth);
-    $(".div1").css("z-index", "101");
-    // $(".div1").css("position", "absolute");
-    $(".div1").css("background-color", "green");
-    $(".div1").css("text-align", "center");
-  });
+  newPos = new Object();
+  newPos.left = dx1;
+  newPos.top = y1;
+  $(".div1").offset(newPos);
+  $(".div1").css("height", calcHeight);
+  parentWidth = $(".day").innerWidth();
+  $(".div1").css("width", parentWidth);
+  $(".div1").css("z-index", "101");
+  $(".div1").css("background-color", "green");
+  $(".div1").css("text-align", "center");
+  $(".div1").css("visibility", "visible");
 }
 // timeTableGrid(".12pm", ".20pm", ".thursday");
-timeTableGrid(".8am", ".9am", ".friday");
+timeTableGrid(".10am", ".19pm", ".wednesday");
+timeTableGrid(".10am", ".19pm", ".sunday");
 // timeTableGrid(".8am", ".9am", ".monday");
